3ffd2c0b48b3ecc1383d7f9bea4f47f0e2a880be
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-11-16 17:47:32 -0500

    successfully connected to mongoDb and server is up running

--- server/server.js
+++ server/server.js
@@ -1,18 +1,58 @@
-require("dotenv").config();
+
 const express = require("express");
-const helmet = require('helmet');
+const helmet = require("helmet");
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 const app = express();
+
+const users = require("./routes/users");
+
+
+// const dotenv = require("dotenv").config( { path: "./config/config.env"});
 const PORT = process.env.PORT || 4001;
 
-app.use(helmet());
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+
+app.use("/api/v1/users/", users);
+
+if(app.get('env' === 'development')){
+  app.use(morgan('dev'));
+  console.log(`Morgan is enabled`);
+}
+
+mongoose.connect("mongodb+srv://jethroglaudin:4534123cC!@yelp.leqzd.mongodb.net/yelp?retryWrites=true&w=majority", {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+})
+.then(console.log("Connected to MongoDB database"))
+.catch((err) => console.log(err));
 
 
-app.get("/getRestaurants", async (req, res) => {
-    console.log('get all restaurants');
-  res.json("get all restaurants");
+// Get all restaurants
+app.get("/api/v1/restaurants", async (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      restaurants: ["mcdonalds", "wendys"],
+    },
+  });
 });
 
-app.listen(PORT, () =>
+app.get("/api/v1/restaurants/:restaurantid", async (req, res) => {
+    console.log(req.params);
+})
+
+
+const server = app.listen(PORT, () =>
   console.log(`Server is currently running on port ${PORT}!`)
 );
+
+process.on("unhandledRejection", (error, promise) => {
+  console.log(`Error: ${error.message}`);
+  server.close(() => process.exit(1));
+})
