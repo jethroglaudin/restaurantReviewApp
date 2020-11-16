bf8841d59e1a587417a70e3d40608591bc7a7208
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-11-15 23:35:09 -0500

    added helmet dependency

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
 require("dotenv").config();
 const express = require("express");
-
+const helmet = require('helmet');
 const app = express();
 const PORT = process.env.PORT || 4001;
 
+app.use(helmet());
 app.use(express.json());
 
 
-app.get("/getRestaurants", async (req, res, next) => {
-
+app.get("/getRestaurants", async (req, res) => {
+    console.log('get all restaurants');
   res.json("get all restaurants");
 });
 
