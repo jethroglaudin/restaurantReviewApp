ead8abbaf86aed2760da56e02ac2d4b2efd305c4
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-11-20 23:05:50 -0500

    added restaurantList

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import AddRestaurant from '../components/AddRestaurant.jsx';
 import Header from '../components/Header.jsx';
+import { RestaurantList } from '../components/RestaurantList.jsx';
 
 const Home = () => {
     return (
         <div>
            <Header />
            <AddRestaurant />
+           <RestaurantList />
         </div>
     )
 }
