ee87c6fac6901690521b16d73d2ac84560f07d5a
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-11-23 23:45:54 -0500

    working on updating restaurants from client end

---
+++ client/src/components/UpdateRestaurant.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const UpdateRestaurant = (props) => {
+  const { id } = useParams();
+
+  return (
+    <div>
+      <form action="">
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input id="name" className="form-control" type="text" />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="location">Location</label>
+          <input id="location" className="form-control" type="text" />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="price_range">Price Range</label>
+          <input id="price_range" className="form-control" type="number" />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default UpdateRestaurant;
