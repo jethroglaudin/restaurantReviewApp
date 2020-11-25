59c0736aa9123fb4537017e42cfd3ce3342fc380
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-11-24 23:17:15 -0500

    still working on update Restaurant component

--- client/src/components/UpdateRestaurant.jsx
+++ client/src/components/UpdateRestaurant.jsx
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 
 const UpdateRestaurant = (props) => {
   const { id } = useParams();
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const [price_range, setPriceRange] = useState("");
 
   return (
     <div>
       <form action="">
         <div className="form-group">
           <label htmlFor="name">Name</label>
-          <input id="name" className="form-control" type="text" />
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            id="name"
+            className="form-control"
+            type="text"
+          />
         </div>
 
         <div className="form-group">
           <label htmlFor="location">Location</label>
-          <input id="location" className="form-control" type="text" />
+          <input
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            id="location"
+            className="form-control"
+            type="text"
+          />
         </div>
 
         <div className="form-group">
           <label htmlFor="price_range">Price Range</label>
-          <input id="price_range" className="form-control" type="number" />
+          <input
+            value={price_range}
+            onChange={(e) => setPriceRange(e.target.value)}
+            id="price_range"
+            className="form-control"
+            type="number"
+          />
         </div>
+        <button className="btn btn-primary">Submit</button>
       </form>
     </div>
   );
