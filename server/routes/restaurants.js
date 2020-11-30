9f62ef886a62842b7c09edd61cc5121f95a1694b
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-11-30 00:51:36 -0500

    forgot to add params for route

--- server/routes/restaurants.js
+++ server/routes/restaurants.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { test, allRestaurants, getRestaurant, 
-    createRestaurant, updateRestaurant,
-     deleteRestaurant, addReview } = require("../controllers/restaurantController");
+const {
+  test,
+  allRestaurants,
+  getRestaurant,
+  createRestaurant,
+  updateRestaurant,
+  deleteRestaurant,
+  addReview,
+} = require("../controllers/restaurantController");
 
 const auth = require("../middleware/auth");
 
@@ -10,8 +16,8 @@ router.get("/test", test);
 router.get("/", allRestaurants);
 router.get("/:id", getRestaurant);
 router.post("/", createRestaurant);
-router.put("/", updateRestaurant);
+router.put("/:id", updateRestaurant);
 router.delete("/:id", deleteRestaurant);
 router.post("/:id/addReview", addReview);
 
-module.exports = router
+module.exports = router;
