8b9988c11dfcd70d74cc77aed8a1344cfa4ec51d
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-11-30 16:28:38 -0500

    added Restaurant Detail component

---
+++ client/src/components/RestaurantDetail.jsx
@@ -0,0 +1,8 @@
+import React, { useContext, useEffect, useState } from "react";
+import { RestaurantsContext } from "../context/RestaurantsContext";
+
+const RestaurantDetail = (props) => {
+  return <div>Hello</div>;
+};
+
+export default RestaurantDetail;
