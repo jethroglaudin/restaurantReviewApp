5ba1cab77bbb6e9a2a0a6c210841b8d0c5924e31
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-11-30 17:10:07 -0500

    so far component is able to retreive the given restaurant data via our api and are displaying the restuarant name to the client

--- client/src/components/RestaurantDetail.jsx
+++ client/src/components/RestaurantDetail.jsx
@@ -1,8 +1,30 @@
 import React, { useContext, useEffect, useState } from "react";
-import { RestaurantsContext } from "../context/RestaurantsContext";
+import { useParams } from "react-router-dom";
+import RestaurantFinder from "../apis/RestaurantFinder";
+import {
+  RestaurantContextProvider,
+  RestaurantsContext,
+} from "../context/RestaurantsContext";
 
-const RestaurantDetail = (props) => {
-  return <div>Hello</div>;
+const RestaurantDetail = () => {
+  const { id } = useParams();
+  const { selectedRestaurant, setSelectedRestaurant } = useContext(
+    RestaurantsContext
+  );
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await RestaurantFinder.get(`/${id}`);
+        // udate our global state
+        setSelectedRestaurant(response.data.data.restaurant);
+        // console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+  return <div>{selectedRestaurant && selectedRestaurant.name}</div>;
 };
 
 export default RestaurantDetail;
