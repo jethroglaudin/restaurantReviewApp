66b096d7134e49415f2d82acac0f15d1ee6a6207
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-12-05 22:39:54 -0500

    working on mapping through reviews array and returing a card with each review for the selected restaurant

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 import StarRating from "./StarRating";
 
-const Reviews = () => {
+const Reviews = ({ reviews }) => {
   return (
     <div className="row row-cols-3 mb-2">
+      {reviews.map((review) => {
+        return (
+          <div
+            className="card text-white bg-primary mb-3 mr-4"
+            style={{ maxWidth: "30%" }}
+          >
+            <div className="card-header d-flex justify-content-between">
+              <span>Joan</span>
+              <span>
+                <StarRating rating={3} />
+              </span>
+            </div>
+            <div className="card-body">
+              <p className="card-text">This restaurant was awesome</p>
+            </div>
+          </div>
+        );
+      })}
+      {/* 
       <div
         className="card text-white bg-primary mb-3 mr-4"
         style={{ maxWidth: "30%" }}
@@ -45,21 +64,7 @@ const Reviews = () => {
         <div className="card-body">
           <p className="card-text">This restaurant was awesome</p>
         </div>
-      </div>
-      <div
-        className="card text-white bg-primary mb-3 mr-4"
-        style={{ maxWidth: "30%" }}
-      >
-        <div className="card-header d-flex justify-content-between">
-          <span>Joan</span>
-          <span>
-            <StarRating rating={3} />
-          </span>
-        </div>
-        <div className="card-body">
-          <p className="card-text">This restaurant was awesome</p>
-        </div>
-      </div>
+      </div> */}
     </div>
   );
 };
