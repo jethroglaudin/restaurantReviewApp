3645c20cf9aa2ced01daed973e71d5bbc4cab1c0
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-12-06 23:14:24 -0500

    rendered out the values we mapped through

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -7,17 +7,18 @@ const Reviews = ({ reviews }) => {
       {reviews.map((review) => {
         return (
           <div
+            key={review.id}
             className="card text-white bg-primary mb-3 mr-4"
             style={{ maxWidth: "30%" }}
           >
             <div className="card-header d-flex justify-content-between">
-              <span>Joan</span>
+              <span>{review.name}</span>
               <span>
-                <StarRating rating={3} />
+                <StarRating rating={review.rating} />
               </span>
             </div>
             <div className="card-body">
-              <p className="card-text">This restaurant was awesome</p>
+              <p className="card-text">{review.review}</p>
             </div>
           </div>
         );
