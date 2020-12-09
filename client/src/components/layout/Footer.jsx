9d36bc9c72151028d03967115baddf855c901816
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-12-08 21:14:40 -0500

    template for footer established. May still change

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>This is the footer</div>;
+  return (
+    <footer className="bg-dark text-white mt-5 p-4 text-center">
+      Copyright &copy; {new Date().getFullYear()} Yelp
+    </footer>
+  );
 };
 
 export default Footer;
