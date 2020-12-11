9054bb6d99b2f385c5fcd45171c36eb8e9f84fb4
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-12-11 14:18:15 -0500

    changed name

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
-    return (
-        <div>
-            <h1 className="font-weight-light display-1 text-center">Restaurant Finder</h1>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1 className="font-weight-light display-1 text-center">Yelp 2.0</h1>
+      <p className="align-text-center text-center">
+        Give feedback on some of your restaurant experiences
+      </p>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
