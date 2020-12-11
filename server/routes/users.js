486fbe6aff049db238056714296fb5f0c9308bac
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-12-11 16:57:51 -0500

    added new route to get current user

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
 
-const { register, login, test } = require("../controllers/userController");
-
+const {
+  register,
+  login,
+  test,
+  currentUser,
+} = require("../controllers/userController");
 
 router.get("/test", test);
+router.get("/", auth, currentUser);
 router.post("/register", register);
 router.post("/login", login);
-
 
 module.exports = router;
