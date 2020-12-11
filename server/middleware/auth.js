63b08621fbdb09fc572194b106fca375e80ccb3f
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-12-11 17:00:57 -0500

    updated auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,14 +1,14 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 module.exports = function authenticate(req, res, next) {
-    const token = req.header("x-auth-token");
-    if(!token) return res.status(401).json({ "message": "Access Denied" });
+  const token = req.header("x-auth-token");
+  if (!token) return res.status(401).json({ message: "Access Denied" });
 
-    try {
-        const decodedPayload = jwt.verify(token, process.env.token)
-        req.user = decodedPayload;
-        next(); 
-    } catch (error) {
-        res.status(400).json({ "message": "Invalid token"});
-    }
-}
+  try {
+    const decodedPayload = jwt.verify(token, process.env.jwtPrivateKey);
+    req.user = decodedPayload;
+    next();
+  } catch (error) {
+    res.status(400).json({ message: "Invalid token" });
+  }
+};
