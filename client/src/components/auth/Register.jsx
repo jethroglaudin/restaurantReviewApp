9f58854e5aa8154691e5ccae1c6b3c05f0e5800a
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-12-11 14:18:51 -0500

    created form that changes state haven't fully implemented loging in feature yet

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -1,9 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  let history = useHistory();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    history.push("/login");
+  };
   return (
     <div>
-      <h1>Register page</h1>
+      <h1 className="text-center">Register</h1>
+      <form action="">
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            id="name"
+            className="form-control"
+            type="text"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="name">Username</label>
+          <input
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+            id="userName"
+            className="form-control"
+            type="text"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            id="email"
+            className="form-control"
+            type="text"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            id="password"
+            className="form-control"
+            type="text"
+          />
+        </div>
+        <button
+          type="submit"
+          onClick={handleSubmit}
+          className="btn btn-primary"
+        >
+          Register
+        </button>
+      </form>
     </div>
   );
 };
