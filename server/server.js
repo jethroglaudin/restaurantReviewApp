0fa2561f477a00e19611f1fe7ac34987ed71b874
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-12-11 16:57:36 -0500

    updated connection to mongoDB

--- server/server.js
+++ server/server.js
@@ -1,14 +1,12 @@
-
 const express = require("express");
 const helmet = require("helmet");
-const morgan = require('morgan');
-const mongoose = require('mongoose');
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 
 const users = require("./routes/users");
 const restaurants = require("./routes/restaurants");
-
 
 // const dotenv = require("dotenv").config( { path: "./config/config.env"});
 const PORT = process.env.PORT || 4001;
@@ -18,27 +16,23 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use("/api/v1/users/", users);
 app.use("/api/v1/restaurants/", restaurants);
 
-if(app.get('env' === 'development')){
-  app.use(morgan('dev'));
+if (app.get("env" === "development")) {
+  app.use(morgan("dev"));
   console.log(`Morgan is enabled`);
 }
 
-mongoose.connect("mongodb+srv://jethroglaudin:4534123cC!@yelp.leqzd.mongodb.net/yelp?retryWrites=true&w=majority", {
-  useNewUrlParser: true, 
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-})
-.then(console.log("Connected to MongoDB database"))
-.catch((err) => console.log(err));
-
-
-
-
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(console.log("Connected to MongoDB database"))
+  .catch((err) => console.log(err));
 
 const server = app.listen(PORT, () =>
   console.log(`Server is currently running on port ${PORT}!`)
@@ -47,4 +41,4 @@ const server = app.listen(PORT, () =>
 process.on("unhandledRejection", (error, promise) => {
   console.log(`Error: ${error.message}`);
   server.close(() => process.exit(1));
-})
+});
