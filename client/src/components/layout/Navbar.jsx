4b89d9158101fbbcc45ba617aa1929624df57090
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-12-18 22:25:46 -0500

    Navbar now changes depending on if user is authenticated. Still have a bug getting the user name from the payload

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -1,39 +1,61 @@
-import React from "react";
+import React, { Fragment, useContext } from "react";
 import { Link, useHistory } from "react-router-dom";
+import AuthContext from "../../context/auth/authContext";
 
 const Navbar = () => {
-  const logout = (e) => {
+  const authContext = useContext(AuthContext);
+
+  const { isAuthenticated, logout, user } = authContext;
+
+  const onLogout = (e) => {
     console.log("logging out");
+    logout();
     // push to login page
     // useHistory()
   };
-  return (
-    <nav className="navbar bg-light">
-      <ul style={{ listStyle: "none" }}>
-        <li>
+
+  const authLinks = (
+    <>
+    <li>
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="#">
+          <Link to="/about">
             <span className="hide-sm">About</span>
           </Link>
         </li>
+      <li>Welcome, {user && user.name} </li>
+      <li>
+        <a onClick={onLogout} href="#!">
+          <i className="fas fa-sign-out-alt" />
+          <span className="hide-sim">Logout</span>
+        </a>
+      </li>
+    </>
+  );
+
+  const guestLinks = (
+    <>
+    <li>
+          <Link to="/">Home</Link>
+        </li>
         <li>
-          <Link to="/login">
-            <span className="hide-sm">Sign In</span>
+          <Link to="/about">
+            <span className="hide-sm">About</span>
           </Link>
         </li>
-        <li>
-          <Link to="/register">
-            <span className="hide-sm">Signup</span>
-          </Link>
-        </li>
-        <li>
-          <a onClick={logout} href="#!">
-            {/* <i className="fas fa-sign-out-alt"></i>{" "} */}
-            <span className="hide-sm">Logout</span>
-          </a>
-        </li>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </>
+  );
+  return (
+    <nav className="navbar bg-light">
+      <ul style={{ listStyle: "none" }}>
+      {isAuthenticated ? authLinks : guestLinks}
       </ul>
     </nav>
   );
