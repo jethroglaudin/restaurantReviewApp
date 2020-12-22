1c84c35bb58aa3aa7563924d6e39f636e96d2d96
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-12-21 23:08:21 -0500

    created route to about page componenent

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import "./App.css";
 import Footer from "./components/layout/Footer";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import About from "./components/layout/About";
 import AuthState from "./context/auth/AuthState";
 import setAuthToken from "./utils/setAuthToken";
 import PrivateRoute from "./components/PrivateRoute";
@@ -28,6 +29,7 @@ const App = () => {
               <Route exact path="/" component={Home} />
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/about" component={About} />
               <PrivateRoute
                 exact
                 path="/restaurants/:id/update"
