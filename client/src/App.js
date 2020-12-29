1469f508543b507ce659ae42ce98a2b25a61a171
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-12-28 21:00:28 -0500

    made path to home component private

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,7 @@ const App = () => {
           <Navbar />
           <div className="container">
             <Switch>
-              <Route exact path="/" component={Home} />
+              <PrivateRoute exact path="/" component={Home} />
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/about" component={About} />
