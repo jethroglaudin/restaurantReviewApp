9ba9aebdb43451e247706c529f8ca74493f27c05
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-12-28 19:50:36 -0500

    incorporated useEffect hook to call loadUser from authContext

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import AddRestaurant from '../components/AddRestaurant.jsx';
 import Header from '../components/Header.jsx';
 import { RestaurantList } from '../components/RestaurantList.jsx';
 import PrivateRoute from "../components/PrivateRoute";
+import AuthContext from "../context/auth/authContext";
 
 const Home = () => {
+    const authContext = useContext(AuthContext);
+
+    useEffect(() => {
+        authContext.loadUser();
+    }, []);
     return (
         <div>
            <Header />
